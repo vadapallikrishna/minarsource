85d22e8cb3e5e5148fe52508225fb9ec123ff41b
Krishna Satya <vadapallikrishna@gmail.com>
2020-08-13 12:08:16 +0530

    new notebook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import { LandingPage } from "./landing/page";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { NewNotebook } from "./newnotebook";
 
 function App() {
   return (
     <div className="App">
+        <Router>
+	<Switch>
+	<Route exact path="/">
 	<LandingPage/>
+	</Route>
+	<Route path="/new">
+	<NewNotebook/>
+	</Route>
+	</Switch>
+	</Router>
     </div>
   );
 }
---
+++ src/newnotebook.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { MainLayout } from "./mainlayout";
+
+export function NewNotebook(props: any) {
+       return <MainLayout>
+       <h4>New Notebook</h4>
+       <Form.Group>
+       	 <Form.Label>Notebook name</Form.Label>
+	 <Form.Control type="text"/>
+       </Form.Group>
+       <Button variant="primary">
+       	Create
+       </Button>
+       </MainLayout>
+}
+
