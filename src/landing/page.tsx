dac69b18ead57b247700734cbf7c8bf04004531d
Krishna Satya <vadapallikrishna@gmail.com>
2020-08-13 14:13:51 +0530

    landing

--- src/landing/page.tsx
+++ src/landing/page.tsx
@@ -17,7 +17,7 @@ export function LandingPage(props: any) {
 		<h5>Search</h5>
 		<p>already written tutorials, snippets</p>
 		</div>
-		<img width="100" height="100" src="/search-24px.svg"/>
+		<img alt="search" width="100" height="100" src="/search-24px.svg"/>
 		</Row>
 		</Container>
         </MainLayout>;
