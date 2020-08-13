07ad525f8eeb3dec0c98679072c7fd52281010f1
Krishna Satya <vadapallikrishna@gmail.com>
2020-08-13 15:31:25 +0530

    codeabout.tsx

--- src/codeabout.tsx
+++ src/codeabout.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import {MainLayout} from "./mainlayout";
+import {Nav} from "react-bootstrap";
 
 export function CodeAbout(props:any) {
       return <MainLayout>
       <section className="section-head">
       <h4>Intro to React</h4>
+      <Nav variant="tabs">
+	<Nav.Item>
+	<Nav.Link>Commits</Nav.Link>
+	</Nav.Item>
+	<Nav.Item>
+	<Nav.Link>Add Extension</Nav.Link>
+	</Nav.Item>
+      </Nav>
       </section>
       </MainLayout>
 }
